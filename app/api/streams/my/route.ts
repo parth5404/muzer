f613cf9fdafa68f86dae46d0f4668fe5123ae04d
Parth Lahoti <parthlahoti5404@gmail.com>
2024-12-27 10:31:57 +0530

    some fixes

--- app/api/streams/my/route.ts
+++ app/api/streams/my/route.ts
@@ -1,29 +1,27 @@
-import { z } from "zod";
-import { getServerSession } from "next-auth";
-import { NextRequest,NextResponse } from "next/server";
-import { prismaclient } from "@/app/lib/db";
+import { NextResponse } from 'next/server';
+import { getSession } from 'next-auth/react';
+import {prismaclient} from '@/app/lib/db';
 
-export default async function GET() {
-  const session = await getServerSession();
-         const user =await prismaclient.user.findUnique({
-             where:{
-                 email:  session?.user?.email??""  
-             }
-         });
-         if(!user){
-            return NextResponse.json({
-                message: "Unauthorized"
-            }, {
-                status: 403
-            }); 
-        }
-        //if user exists find the stream for the user
-        const streams=await prismaclient.stream.findMany({
-            where:{
-                userId:user.id
-            }
-        });
-        return NextResponse.json({
-            streams
-        });
+export async function GET(request: Request) {
+  const session = await getSession();
+  const user = session?.user as { id: string; name?: string | null; email?: string | null; image?: string | null };
+
+  if (!user) {
+    return NextResponse.json({
+      message: "Unauthorized"
+    }, {
+      status: 403
+    });
+  }
+
+  // If user exists, find the stream for the user
+  const streams = await prismaclient.stream.findMany({
+    where: {
+      userId: user.id
+    }
+  });
+
+  return NextResponse.json({
+    streams
+  });
 }
